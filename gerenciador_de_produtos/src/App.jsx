77c09c0d06bbb896a64502dce5d8b61f13a1b270
JulioCAC <juliocac999@gmail.com>
2024-07-27 16:02:42 -0300

    navbar criada

--- gerenciador_de_produtos/src/App.jsx
+++ gerenciador_de_produtos/src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import FornecedorList from './pages/fornecedor/FornecedorList'
+import Navbar from './components/Navbar'
 const App = () => {
   return (
     <BrowserRouter>
+    <Navbar/>
+    <div className="container">
       <Routes>
         <Route path="/" element={<FornecedorList/>}/>
         <Route path="/listar-fornecedores" element={<FornecedorList/>}/>
       </Routes>
+    </div>
+      
     </BrowserRouter>
   )
 }
