ab0d4c142cb42a3c2281cfeb62667684e826876a
JulioCAC <juliocac999@gmail.com>
2024-08-10 15:47:34 -0300

    ajuste na nav bar e criaca da pagina de inicio

--- gerenciador_de_produtos/src/App.jsx
+++ gerenciador_de_produtos/src/App.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import FornecedorList from './pages/fornecedor/FornecedorList'
 import Navbar from './components/Navbar'
+
 import FornecedorForm from './pages/fornecedor/FornecedorForm'
 import ProdutoList from './pages/produto/ProdutoList'
 import ProdutoForm from './pages/produto/ProdutoForm'
+import Inicio from './pages/Inicio'
 const App = () => {
   return (
     <BrowserRouter>
     <Navbar/>
     <div className="container">
       <Routes>
-        <Route path="/" element={<FornecedorList/>}/>
+        <Route path="/" element={<Inicio/>}/>
         <Route path="/listar-fornecedores" element={<FornecedorList/>}/>
         <Route path="/add-fornecedores" element={<FornecedorForm/>}/>
         <Route path="edit-fornecedores/:id" element={<FornecedorForm/>}/>
@@ -20,6 +22,7 @@ const App = () => {
         <Route path="/add-produtos" element={<ProdutoForm />} />
         <Route path="edit-produtos/:id" element={<ProdutoForm/>}/>
       </Routes>
+      
     </div>
       
     </BrowserRouter>
