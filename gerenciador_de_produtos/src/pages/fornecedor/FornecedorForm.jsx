ba6af5753fee4ca4cc4cbf8741dce1bd61cea50a
JulioCAC <juliocac999@gmail.com>
2024-08-10 09:47:14 -0300

    alteracao no css

---
+++ gerenciador_de_produtos/src/pages/fornecedor/FornecedorForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const FornecedorForm = () => {
+    const [fornecedor, setFornecedor] = useState({nome: "", cnpj: "", email: ""})
+  return (
+    <div>FornecedorForm</div>
+  )
+}
+
+export default FornecedorForm
