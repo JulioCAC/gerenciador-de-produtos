cf1f290c27ccd8b953c43c5d54e5ffabb1d75f70
JulioCAC <juliocac999@gmail.com>
2024-08-27 16:13:13 -0300

    criacao e conclusao do ClienteList

--- gerenciador_de_produtos/src/App.jsx
+++ gerenciador_de_produtos/src/App.jsx
@@ -1,31 +1,31 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import FornecedorList from './pages/fornecedor/FornecedorList'
 import Navbar from './components/Navbar'
-
 import FornecedorForm from './pages/fornecedor/FornecedorForm'
 import ProdutoList from './pages/produto/ProdutoList'
 import ProdutoForm from './pages/produto/ProdutoForm'
-import Inicio from './pages/Inicio'
+import Inicial from './pages/Inicio'
+import ClienteList from './pages/cliente/ClienteList'
 import ClienteForm from './pages/cliente/ClienteForm'
+
 const App = () => {
   return (
     <BrowserRouter>
-    <Navbar/>
+    <Navbar />
     <div className="container">
-      <Routes>
-        <Route path="/" element={<Inicio/>}/>
-        <Route path="/listar-fornecedores" element={<FornecedorList/>}/>
-        <Route path="/add-fornecedores" element={<FornecedorForm/>}/>
-        <Route path="edit-fornecedores/:id" element={<FornecedorForm/>}/>
-        <Route path="listar-produtos" element={<ProdutoList/>}/>
+    <Routes>
+        <Route path="/" element={<Inicial />} />
+        <Route path="/listar-fornecedores" element={<FornecedorList />} />
+        <Route path="/add-fornecedores" element={<FornecedorForm />} />
+        <Route path="/edit-fornecedores/:id" element={<FornecedorForm />} />
         <Route path="/listar-produtos" element={<ProdutoList />} />
         <Route path="/add-produtos" element={<ProdutoForm />} />
-        <Route path="edit-produtos/:id" element={<ProdutoForm/>}/>
-        <Route path="/add-clientes" element={<ClienteForm/>}/>
+        <Route path="/add-clientes" element={<ClienteForm />} />
+        <Route path="/listar-clientes" element={<ClienteList />} />
+        <Route path="/edit-clientes/:id" element={<ClienteForm />} />
       </Routes>
     </div>
-      
     </BrowserRouter>
   )
 }
